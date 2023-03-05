a954de99f8b39e70e5c7eeabe74887424c620a6e
Ramil Girfanov <Ramil.Girfanov@gmail.com>
2023-03-05 23:32:52 +0300

    Экран входа готов

---
+++ CryptoWallet/CryptoWallet/LoginScreen/LoginScreen.swift
@@ -0,0 +1,69 @@
+//
+//  LoginScreen.swift
+//  CryptoWallet
+//
+//  Created by Рамиль Гирфанов on 05.03.2023.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //    MARK: - UIObjects
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Вход"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let stack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    let loginTextField: UITextField = {
+        let loginTextField = UITextField()
+        loginTextField.placeholder = "Введите логин"
+        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        return loginTextField
+    }()
+    
+    let passTextField: UITextField = {
+        let passTextField = UITextField()
+        passTextField.placeholder = "Введите пароль"
+        passTextField.translatesAutoresizingMaskIntoConstraints = false
+        return passTextField
+    }()
+    
+    
+//    MARK: - Layout
+    
+    private func layout() {
+        [label, stack].forEach { addSubview($0) }
+        
+        [loginTextField, passTextField].forEach { stack.addSubview($0) }
+        
+        let safeIndent: CGFloat = 16
+        
+        NSLayoutConstraint.activate([
+            stack.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stack.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: safeIndent),
+            stack.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: safeIndent),
+            
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.bottomAnchor.constraint(equalTo: stack.topAnchor, constant: -safeIndent)
+        ])
+    }
+}

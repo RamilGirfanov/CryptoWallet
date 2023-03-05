473ed8614b27051af3afd4b81fc8dc755fffae48
Ramil Girfanov <Ramil.Girfanov@gmail.com>
2023-03-05 23:50:02 +0300

    Добавлен NavigationController

---
+++ CryptoWallet/CryptoWallet/System/NavigationController.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationController.swift
+//  CryptoWallet
+//
+//  Created by Рамиль Гирфанов on 05.03.2023.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    override func loadView() {
+        setupNC()
+    }
+    
+    private func setupNC() {        
+        
+    }
+}

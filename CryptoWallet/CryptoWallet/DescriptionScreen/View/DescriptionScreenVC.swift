9b817eda9a3263df81fbec3af95b75622214c9a8
Ramil Girfanov <Ramil.Girfanov@gmail.com>
2023-03-10 17:58:59 +0300

    Подготовка к экрану описания

---
+++ CryptoWallet/CryptoWallet/DescriptionScreen/View/DescriptionScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  DescriptionScreenVC.swift
+//  CryptoWallet
+//
+//  Created by Рамиль Гирфанов on 10.03.2023.
+//
+
+import UIKit
+
+class DescriptionScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
